4a8da4323229358191aaf702f2550b724d74046b
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2022-09-23 17:52:11 +0200

    heroku script change

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.all("*", (req, res) => {
   res.status(400).json("Route introuvable ! 🦒");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
